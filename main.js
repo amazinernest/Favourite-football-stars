70ed9a7db6dd7c993a52a0a17e0ba94faeede0d6
amazinernest <77410231+amazinernest@users.noreply.github.com>
2022-07-20 21:07:35 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,52 @@
+document.getElementById("RobertoCarlos").onclick = ShowCarlos;
+document.getElementById("Lahm").onclick = ShowLahm;
+document.getElementById("Gerard").onclick = ShowGerard;
+document.getElementById("Pirlo").onclick = ShowPirlo;
+document.getElementById("Kaka").onclick = ShowKaka;
+document.getElementById("Terry").onclick = ShowTerry;
+document.getElementById("Rivaldo").onclick = ShowRivaldo;
+document.getElementById("Zidane").onclick = ShowZidane;
+document.getElementById("Ronaldinho").onclick = ShowRonaldinho;
+
+function ShowCarlos() {
+  document.querySelector("body").style.background =
+    "url(Images/04E037370000044D-5081637-image-m-45_1510671077280.jpg)";
+}
+
+function ShowLahm() {
+  document.querySelector("body").style.background = "url(Images/6071.jpg)";
+}
+
+function ShowGerard() {
+  document.querySelector("body").style.background =
+    "url(Images/1975200-41499487-2560-1440.jpg)";
+}
+
+function ShowPirlo() {
+  document.querySelector("body").style.background =
+    "url(Images/andrea-pirlo-italy-costa-rica_3163056.jpg)";
+}
+
+function ShowKaka() {
+  document.querySelector("body").style.background =
+    "url(Images/GettyImages-481333713.jpg)";
+}
+
+function ShowRivaldo() {
+  document.querySelector("body").style.background = "url(Images/rivaldo-4.jpg)";
+}
+
+function ShowTerry() {
+  document.querySelector("body").style.background =
+    "url(Images/john-terry_1564470425.jpg)";
+}
+
+function ShowZidane() {
+  document.querySelector("body").style.background =
+    "url(Images/Zinedine-Zidane.jpg)";
+}
+
+function ShowRonaldinho() {
+  document.querySelector("body").style.background =
+    "url(Images/Ronaldinho-Barcelona-752x428.jpg)";
+}
